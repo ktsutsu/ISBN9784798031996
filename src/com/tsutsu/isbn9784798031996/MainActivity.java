1f0c4bfe028ce80a844f7ed3695635e1752ffe0e
ktsutsu <tsutsu0422@googlemail.com>
2012-08-11 22:18:37 +0900

    Change EventListener
    [Chapter2.3.7]

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,9 +2,10 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.*;
 import android.view.Menu;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.*;
 
 public class MainActivity extends Activity {
 
@@ -12,16 +13,20 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btn = (Button)this.findViewById(R.id.button1);
+        btn.setOnClickListener(
+        	new View.OnClickListener() {
+				public void onClick(View v) {
+					TextView text1 = (TextView)findViewById(R.id.textView1);
+					text1.setText("OK!");
+				}
+			}
+        );
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.activity_main, menu);
         return true;
-    }
-
-    public void doAction(View view) {
-    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
-    	text1.setText("OK!");
     }
 }
