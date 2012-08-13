c801fa73ca1284617a424d76eedacf25dfd7742d
ktsutsu <tsutsu0422@googlemail.com>
2012-08-13 12:31:44 +0900

    3.1.3 RadioButtonとRadioGroup

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -3,7 +3,7 @@ package com.tsutsu.isbn9784798031996;
 import android.os.Bundle;
 import android.app.Activity;
 import android.widget.*;
-import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -12,13 +12,13 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Activity activity = this;
-        ToggleButton toggle1 = (ToggleButton)this.findViewById(R.id.toggleButton1);
-        toggle1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-			public void onCheckedChanged(CompoundButton buttonView,
-					boolean isChecked) {
-				Toast toast = Toast.makeText(activity, "You checked: " + isChecked, Toast.LENGTH_SHORT);
+        RadioGroup group1 = (RadioGroup)this.findViewById(R.id.radioGroup1);
+        group1.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			public void onCheckedChanged(RadioGroup group, int checkedId) {
+				RadioButton btn = (RadioButton)findViewById(checkedId);
+				Toast toast = Toast.makeText(activity, "You checked: " + btn.getText(), Toast.LENGTH_SHORT);
 				toast.show();
 			}
-        });
+		});
     }
 }
