fe27519eb37f973ab16a209b0dd2e35ad10e2c93
ktsutsu <tsutsu0422@googlemail.com>
2012-08-13 19:25:28 +0900

    3.3.2 ProgressBarについて

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,8 +2,8 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
 import android.widget.*;
-import android.widget.RatingBar.OnRatingBarChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -11,15 +11,19 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Activity activity = this;
-        RatingBar rate1 = (RatingBar)this.findViewById(R.id.ratingBar1);
-        rate1.setOnRatingBarChangeListener(new OnRatingBarChangeListener() {
-			
-			public void onRatingChanged(RatingBar ratingBar, float rating,
-					boolean fromUser) {
-				Toast toast = Toast.makeText(activity, "rate: " + rating, Toast.LENGTH_SHORT);
-				toast.show();
-			}
-		});
+    }
+
+    public void doAction(View view) {
+    	ProgressBar bar2 = (ProgressBar)this.findViewById(R.id.progressBar2);
+    	int val = bar2.getProgress();
+    	if (val == bar2.getMax()) {
+    		bar2.setIndeterminate(true);
+    	}
+    	if (val == 0) {
+    		bar2.setIndeterminate(false);
+    	}
+    	bar2.setProgress(++val);
+    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
+    	text1.setText("value: " + bar2.getProgress());
     }
 }
