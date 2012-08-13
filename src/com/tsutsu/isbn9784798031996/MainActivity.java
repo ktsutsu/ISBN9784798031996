d32c8122feff179de897a5594c68e0cca469e7bf
ktsutsu <tsutsu0422@googlemail.com>
2012-08-13 16:25:52 +0900

    3.2.5 OnSeekBarChangeListenerによるイベント処理

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,9 +2,8 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.View;
 import android.widget.*;
-import android.widget.AdapterView;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +12,24 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Activity activity = this;
+        final TextView text1 = (TextView)this.findViewById(R.id.textView1);
+        SeekBar bar1 = (SeekBar)this.findViewById(R.id.seekBar1);
+        bar1.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+			
+			public void onStopTrackingTouch(SeekBar seekBar) {
+				Toast toast = Toast.makeText(activity, "Stop...", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+			
+			public void onStartTrackingTouch(SeekBar seekBar) {
+				Toast toast = Toast.makeText(activity, "Start!", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+			
+			public void onProgressChanged(SeekBar seekBar, int progress,
+					boolean fromUser) {
+				text1.setText("value: " + progress);
+			}
+		});
     }
 }
