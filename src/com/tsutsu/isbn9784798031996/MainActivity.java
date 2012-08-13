e9c79ca09079dbd92d2e71f09b2772a0df7987d0
ktsutsu <tsutsu0422@googlemail.com>
2012-08-13 16:13:08 +0900

    3.2.5 SeekBarによるスライダーの利用

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -13,23 +13,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Activity activity = this;
-        Spinner spinner = (Spinner)this.findViewById(R.id.spinner1);
-        String[] items = {"One", "Two", "Three"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, items);
-        spinner.setAdapter(adapter);
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				Spinner spinner = (Spinner)arg0;
-				String str = (String)spinner.getSelectedItem();
-				Toast toast = Toast.makeText(activity, "you selected: " + str, Toast.LENGTH_SHORT);
-				toast.show();
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-				Toast toast = Toast.makeText(activity, "NOT selected...", Toast.LENGTH_SHORT);
-				toast.show();
-			}
-		});
     }
 }
