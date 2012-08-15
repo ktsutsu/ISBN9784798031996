74556e2056ea5fbea44d599be1c1964b208277a9
ktsutsu <tsutsu0422@googlemail.com>
2012-08-15 11:52:09 +0900

    3.4.1 長押しイベントについて

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,9 +2,9 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.text.InputType;
-import android.text.method.PasswordTransformationMethod;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.View.OnLongClickListener;
 import android.widget.*;
 
 public class MainActivity extends Activity {
@@ -13,15 +13,22 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void doAction1(View view) {
-    	EditText text1 = (EditText)this.findViewById(R.id.editText1);
-    	text1.setInputType(InputType.TYPE_CLASS_NUMBER);
-    }
-
-    public void doAction2(View view) {
-    	EditText text1 = (EditText)this.findViewById(R.id.editText1);
-    	text1.setTransformationMethod(PasswordTransformationMethod.getInstance());
+        final Activity activity = this;
+        Button button = (Button)this.findViewById(R.id.button1);
+        button.setOnClickListener(new OnClickListener() {
+			
+			public void onClick(View v) {
+				Toast toast = Toast.makeText(activity, "on Click!", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
+        button.setOnLongClickListener(new OnLongClickListener() {
+			
+			public boolean onLongClick(View v) {
+				Toast toast = Toast.makeText(activity, "on Long Click...", Toast.LENGTH_SHORT);
+				toast.show();
+				return true;
+			}
+		});
     }
 }
