b3bba01b50c25cced73f829ab6156a508ee5b590
ktsutsu <tsutsu0422@googlemail.com>
2012-08-15 12:47:52 +0900

    3.4.2 画面の回転

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,9 +2,8 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.Editable;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.View.OnLongClickListener;
 import android.widget.*;
 
 public class MainActivity extends Activity {
@@ -13,22 +12,26 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Activity activity = this;
-        Button button = (Button)this.findViewById(R.id.button1);
-        button.setOnClickListener(new OnClickListener() {
-			
-			public void onClick(View v) {
-				Toast toast = Toast.makeText(activity, "on Click!", Toast.LENGTH_SHORT);
-				toast.show();
-			}
-		});
-        button.setOnLongClickListener(new OnLongClickListener() {
-			
-			public boolean onLongClick(View v) {
-				Toast toast = Toast.makeText(activity, "on Long Click...", Toast.LENGTH_SHORT);
-				toast.show();
-				return true;
-			}
-		});
     }
+
+    public void doAction(View view) {
+    	EditText edit1 = (EditText)this.findViewById(R.id.editText1);
+    	Editable str = edit1.getText();
+    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
+    	text1.setText("you typed: " + str);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
+    	outState.putString("text1", (String)text1.getText());
+    	super.onSaveInstanceState(outState);
+    }
+
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
+    	text1.setText(savedInstanceState.getString("text1"));
+		super.onRestoreInstanceState(savedInstanceState);
+	}
 }
