4622ee4bc3ba2a07e4a7fbc877f737bce96d7ef8
ktsutsu <tsutsu0422@googlemail.com>
2012-08-17 18:55:42 +0900

    4.1.6 リストの複数項目の選択

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -20,19 +20,31 @@ public class MainActivity extends Activity {
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	builder.setTitle("Message");
     	final CharSequence[] items = {"Android", "iPhone", "Windows Phone"};
-    	final int[] sel_item = {0};
-    	builder.setSingleChoiceItems(items, 0, new DialogInterface.OnClickListener() {
+    	final boolean[] item_checked = {false, false, false};
+    	builder.setMultiChoiceItems(items, item_checked, new DialogInterface.OnMultiChoiceClickListener() {
 			
-			public void onClick(DialogInterface dialog, int which) {
-				sel_item[0] = which;
-				Toast toast = Toast.makeText(activity, "you selected: " + which, Toast.LENGTH_SHORT);
-				toast.show();
+			public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+				item_checked[which] = isChecked;
+				if (isChecked) {
+					Toast toast = Toast.makeText(activity, "you checked: " + which, Toast.LENGTH_SHORT);
+					toast.show();
+				}
 			}
 		});
     	builder.setNeutralButton("OK!", new DialogInterface.OnClickListener() {
 			
 			public void onClick(DialogInterface dialog, int which) {
-				String msg = "selected: " + items[sel_item[0]];
+				String msg = "*selected item*";
+				boolean is_checked = false;
+				for (int i = 0; i < items.length; i++) {
+					if (item_checked[i] == true) {
+						is_checked = true;
+						msg += "\n" + items[i];
+					}
+				}
+				if (!is_checked) {
+					msg = "no items selected...";
+				}
 				Toast toast = Toast.makeText(activity, msg, Toast.LENGTH_SHORT);
 				toast.show();
 			}
