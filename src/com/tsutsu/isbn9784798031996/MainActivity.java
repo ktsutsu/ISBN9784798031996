1b5934788e9c44ddacdce4da5a5f55366f5a1306
ktsutsu <tsutsu0422@googlemail.com>
2012-08-17 17:38:53 +0900

    4.1.4 ダイアログにリストを表示する

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,9 +1,7 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
-import android.widget.EditText;
 import android.widget.Toast;
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -19,38 +17,16 @@ public class MainActivity extends Activity {
 
     public void doAction(View view) {
     	final Activity activity = this;
-    	EditText text1 = (EditText)this.findViewById(R.id.editText1);
-    	Editable str = text1.getText();
     	AlertDialog.Builder builder = new AlertDialog.Builder(this);
     	builder.setTitle("Message");
-    	builder.setMessage("you typed: " + str);
-    	DialogInterface.OnClickListener listener = new DialogInterface.OnClickListener() {
+    	final CharSequence[] items = {"Android", "iPhone", "Windows Phone"};
+    	builder.setItems(items, new DialogInterface.OnClickListener() {
 			
 			public void onClick(DialogInterface dialog, int which) {
-				String msg = "";
-				switch (which) {
-				case DialogInterface.BUTTON_POSITIVE:
-					msg = "OK!!!!";
-					break;
-
-				case DialogInterface.BUTTON_NEUTRAL:
-					msg = "...?";
-					break;
-
-				case DialogInterface.BUTTON_NEGATIVE:
-					msg = "canceled...";
-					break;
-
-				default:
-					break;
-				}
-				Toast toast = Toast.makeText(activity, msg, Toast.LENGTH_SHORT);
+				Toast toast = Toast.makeText(activity, "You selected: " + items[which], Toast.LENGTH_SHORT);
 				toast.show();
 			}
-		};
-		builder.setPositiveButton("OK", listener);
-		builder.setNeutralButton("...", listener);
-		builder.setNegativeButton("Cancel", listener);
-		builder.show();
+		});
+    	builder.show();
     }
 }
