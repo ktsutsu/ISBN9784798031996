471ddf03cd9168a648ed6c660279c68060b393e3
ktsutsu <tsutsu0422@googlemail.com>
2012-08-17 20:05:01 +0900

    4.2.2 DatePickerDialogを利用する

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -3,9 +3,9 @@ package com.tsutsu.isbn9784798031996;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.DatePicker;
-import android.widget.TimePicker;
 import android.widget.Toast;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 
 public class MainActivity extends Activity {
 
@@ -16,11 +16,16 @@ public class MainActivity extends Activity {
     }
 
     public void doAction(View view) {
-    	DatePicker picker1 = (DatePicker)this.findViewById(R.id.datePicker1);
-    	TimePicker picker2 = (TimePicker)this.findViewById(R.id.timePicker1);
-    	String date = String.format("%1$04d-%2$02d-%3$02d", picker1.getYear(), (picker1.getMonth() + 1), picker1.getDayOfMonth());
-    	String time = String.format("%1$02d:%2$02d", picker2.getCurrentHour(), picker2.getCurrentMinute());
-    	Toast toast = Toast.makeText(this, date + "\n" + time, Toast.LENGTH_SHORT);
-    	toast.show();
+    	final Activity activity = this;
+    	DatePickerDialog dlog = new DatePickerDialog(this, new DatePickerDialog.OnDateSetListener() {
+			
+			public void onDateSet(DatePicker view, int year, int monthOfYear,
+					int dayOfMonth) {
+				String msg = String.format("%1$04d-%2$02d-%3$02d", year, (monthOfYear + 1), dayOfMonth);
+				Toast toast = Toast.makeText(activity, msg, Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		}, 2012, 0, 1);
+    	dlog.show();
     }
 }
