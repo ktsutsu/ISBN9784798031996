5e68cd08d5162ff592f7e3c2487c8865cfb9efb9
ktsutsu <tsutsu0422@googlemail.com>
2012-08-20 20:56:34 +0900

    5.1.3 RelativeLayoutによるレイアウト

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -10,4 +12,10 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void doAction(View view) {
+		Button btn = (Button)view;
+		btn.setWidth(btn.getWidth()*2);
+		btn.setHeight(btn.getHeight()*2);
+	}
 }
