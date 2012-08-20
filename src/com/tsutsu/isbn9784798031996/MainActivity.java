ad615ddb8bf2cdeed1725e3ef56d12a073744f55
ktsutsu <tsutsu0422@googlemail.com>
2012-08-20 21:48:49 +0900

    5.1.7 GridViewについて

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -2,8 +2,10 @@ package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TableRow;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -12,5 +14,18 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Activity activity = this;
+        String[] items = {"One", "Two", "Three", "Four", "Five", "Six", "Seven"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.item, items);
+        GridView view1 = (GridView)this.findViewById(R.id.gridView1);
+        view1.setAdapter(adapter);
+        view1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				Toast toast = Toast.makeText(activity, "you clicked: " + arg2, Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
     }
 }
