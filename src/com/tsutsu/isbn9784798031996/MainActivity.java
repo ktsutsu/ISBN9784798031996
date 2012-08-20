fb099c0e069eac9ea9b1a0feaf67d5debaf19a06
ktsutsu <tsutsu0422@googlemail.com>
2012-08-20 18:41:31 +0900

    4.3.3 コンテキストメニューについて

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,10 +1,14 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
-import android.view.Menu;
+import android.view.ContextMenu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.app.Activity;
+import android.app.AlertDialog;
 
 public class MainActivity extends Activity {
 
@@ -12,41 +16,32 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView text1 = (TextView)this.findViewById(R.id.textView1);
+        this.registerForContextMenu(text1);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-    	super.onCreateOptionsMenu(menu);
-    	menu.add("menu 1");
-    	menu.add("menu 2");
-    	return true;
-    }
-
-    @Override
-    public boolean onMenuOpened(int featureId, Menu menu) {
-    	Toast toast = Toast.makeText(this, "open menu...", Toast.LENGTH_SHORT);
-    	toast.show();
-    	return super.onMenuOpened(featureId, menu);
-    }
-
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-    	Toast toast = Toast.makeText(this, "select menu: " + item.getTitle(), Toast.LENGTH_SHORT);
-    	toast.show();
-    	return super.onMenuItemSelected(featureId, item);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-    	Toast toast = Toast.makeText(this, "select option menu: " + item.getTitle(), Toast.LENGTH_SHORT);
-    	toast.show();
-    	return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onOptionsMenuClosed(Menu menu) {
-    	Toast toast = Toast.makeText(this, "close menu!", Toast.LENGTH_SHORT);
-    	toast.show();
-    	super.onOptionsMenuClosed(menu);
+    public void onCreateContextMenu(ContextMenu menu, View v,
+    		ContextMenuInfo menuInfo) {
+    	final Activity activity = this;
+    	menu.setHeaderTitle("Context menu");
+    	menu.add("menu 1").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			
+			public boolean onMenuItemClick(MenuItem item) {
+				Toast toast = Toast.makeText(activity, "This is Menu1.", Toast.LENGTH_SHORT);
+				toast.show();
+				return false;
+			}
+		});
+    	menu.add("menu 2").setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			
+			public boolean onMenuItemClick(MenuItem item) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+				builder.setMessage("This is Menu2.");
+				builder.show();
+				return false;
+			}
+		});
+    	super.onCreateContextMenu(menu, v, menuInfo);
     }
 }
