822ed1b3fada13599182d28040192254fd9da0b0
ktsutsu <tsutsu0422@googlemail.com>
2012-08-20 18:02:03 +0900

    4.3.1 オプションメニューの利用

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,9 +1,11 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
-import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.app.Activity;
-import android.app.ProgressDialog;
+import android.app.AlertDialog;
 
 public class MainActivity extends Activity {
 
@@ -13,13 +15,29 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
     }
 
-    public void doAction(View view) {
-    	final ProgressDialog dlog = new ProgressDialog(this);
-    	dlog.setTitle("Progress...");
-    	dlog.setMessage("please wait...");
-    	dlog.setMax(100);
-    	dlog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
-    	dlog.show();
-    	dlog.incrementProgressBy(50);
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+    	super.onCreateOptionsMenu(menu);
+    	final Activity activity = this;
+    	MenuItem item1 = menu.add("menu 1");
+    	MenuItem item2 = menu.add("menu 2");
+    	item1.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			
+			public boolean onMenuItemClick(MenuItem item) {
+				Toast toast = Toast.makeText(activity, "This is Menu1.", Toast.LENGTH_SHORT);
+				toast.show();
+				return false;
+			}
+		});
+    	item2.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+			
+			public boolean onMenuItemClick(MenuItem item) {
+				AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+				builder.setMessage("This is Menu2.");
+				builder.show();
+				return false;
+			}
+		});
+    	return true;
     }
 }
