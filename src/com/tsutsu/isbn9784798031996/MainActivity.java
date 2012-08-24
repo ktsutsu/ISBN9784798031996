6b21ea7323ffb47735ca20fac774250b37841fed
ktsutsu <tsutsu0422@googlemail.com>
2012-08-24 11:40:54 +0900

    5.2.2 SlidingDrawerの開閉イベント処理

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,6 +1,8 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
+import android.widget.SlidingDrawer;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -9,5 +11,21 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Activity activity = this;
+        SlidingDrawer drawer = (SlidingDrawer)this.findViewById(R.id.slidingDrawer1);
+        drawer.setOnDrawerOpenListener(new SlidingDrawer.OnDrawerOpenListener() {
+			
+			public void onDrawerOpened() {
+				Toast toast = Toast.makeText(activity, "open drawer!", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
+        drawer.setOnDrawerCloseListener(new SlidingDrawer.OnDrawerCloseListener() {
+			
+			public void onDrawerClosed() {
+				Toast toast = Toast.makeText(activity, "close drawer...", Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		});
     }
 }
