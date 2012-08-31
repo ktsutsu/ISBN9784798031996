27627aa4535a8271f83636bd085dbfc87e68aa8d
ktsutsu <tsutsu0422@googlemail.com>
2012-08-31 11:54:51 +0900

    5.3.2 コードから項目を設定する

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,6 +1,12 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
@@ -9,5 +15,19 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final Activity activity = this;
+        String[] arr = {"Windows", "Mac", "Linux"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+        final ListView list = (ListView)this.findViewById(R.id.listView1);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+					long arg3) {
+				TextView text1 = (TextView)arg1;
+				Toast toast = Toast.makeText(activity, "you selected: " + text1.getText(), Toast.LENGTH_SHORT);
+				toast.show();
+			}
+        });
     }
 }
