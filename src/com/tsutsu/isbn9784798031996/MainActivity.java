8663d95ed496f7b0ab1b62bf4d0d0f6a2426b6c7
ktsutsu <tsutsu0422@googlemail.com>
2012-09-03 18:21:52 +0900

    5.3.3 リスト項目の追加・削除

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,33 +1,44 @@
 package com.tsutsu.isbn9784798031996;
 
+import java.util.*;
+
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 import android.app.Activity;
 
 public class MainActivity extends Activity {
+	private int counter = 0;
+	private ArrayAdapter<String> arrayadapter = null;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final Activity activity = this;
-        String[] arr = {"Windows", "Mac", "Linux"};
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
+        List<String> arr = new ArrayList<String>();
+        arr.add("First item");
+        arrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arr);
         final ListView list = (ListView)this.findViewById(R.id.listView1);
-        list.setAdapter(adapter);
+        list.setAdapter(arrayadapter);
+
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
 					long arg3) {
-				TextView text1 = (TextView)arg1;
-				Toast toast = Toast.makeText(activity, "you selected: " + text1.getText(), Toast.LENGTH_SHORT);
-				toast.show();
+				arrayadapter.add("No," + ++counter);
 			}
-        });
+		});
+        list.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+
+			public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
+					int arg2, long arg3) {
+				TextView text = (TextView)arg1;
+				arrayadapter.remove((String)text.getText());
+				return true;
+			}
+		});
     }
 }
