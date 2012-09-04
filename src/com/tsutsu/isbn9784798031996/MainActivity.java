443bffb48850d1203ba46532bc6e0755e1708dc0
ktsutsu <tsutsu0422@googlemail.com>
2012-09-04 16:50:17 +0900

    5.3.6 CheckTextViewによるリスト項目

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -4,10 +4,9 @@ import java.util.*;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.CheckedTextView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
 import android.app.ListActivity;
 
@@ -17,7 +16,6 @@ public class MainActivity extends ListActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        final ListActivity activity = this;
         ListView list = this.getListView();
         list.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
         List<String> arr = new ArrayList<String>();
@@ -26,17 +24,13 @@ public class MainActivity extends ListActivity {
         arr.add("Third item");
         arrayadapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_single_choice, arr);
         this.setListAdapter(arrayadapter);
-        list.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+    }
 
-			public void onItemSelected(AdapterView<?> arg0, View arg1,
-					int arg2, long arg3) {
-				TextView text = (TextView)arg1;
-				Toast toast = Toast.makeText(activity, "you selected: " + arg2 + ": " + text.getText(), Toast.LENGTH_SHORT);
-				toast.show();
-			}
-
-			public void onNothingSelected(AdapterView<?> arg0) {
-			}
-		});
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+    	CheckedTextView check = (CheckedTextView)v;
+    	Toast toast = Toast.makeText(this, "you selected: " + check.getText(), Toast.LENGTH_SHORT);
+    	toast.show();
+    	super.onListItemClick(l, v, position, id);
     }
 }
