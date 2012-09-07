7d6504699f9b596af75ca2cb02b91fd95243b667
ktsutsu <tsutsu0422@googlemail.com>
2012-09-07 18:31:35 +0900

    6.1.4 HelloAppActivityの修正

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,7 +1,11 @@
 package com.tsutsu.isbn9784798031996;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.text.Editable;
+import android.view.View;
+import android.app.*;
+import android.widget.*;
+import android.content.*;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +13,25 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void doAction(View view) {
+    	final Activity activity = this;
+    	EditText edit1 = (EditText)this.findViewById(R.id.editText1);
+    	Editable str = edit1.getText();
+    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
+    	text1.setText("you typed: " + str);
+    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
+    	builder.setTitle("Message");
+    	builder.setMessage("change activity.");
+    	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+			
+			public void onClick(DialogInterface dialog, int which) {
+				Intent intent = new Intent(activity, com.tsutsu.isbn9784798031996.OtherActivity.class);
+				activity.startActivity(intent);
+			}
+		});
+    	builder.setNegativeButton("Cancel", null);
+    	builder.show();
     }
 }
