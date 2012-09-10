28305795888cbe41df81e625646e197e4c505ff2
ktsutsu <tsutsu0422@googlemail.com>
2012-09-10 12:23:28 +0900

    6.1.7 暗示的インテントについて

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -1,7 +1,7 @@
 package com.tsutsu.isbn9784798031996;
 
+import android.net.Uri;
 import android.os.Bundle;
-import android.text.Editable;
 import android.view.View;
 import android.app.*;
 import android.widget.*;
@@ -16,22 +16,9 @@ public class MainActivity extends Activity {
     }
 
     public void doAction(View view) {
-    	final Activity activity = this;
     	EditText edit1 = (EditText)this.findViewById(R.id.editText1);
-    	Editable str = edit1.getText();
-    	TextView text1 = (TextView)this.findViewById(R.id.textView1);
-    	text1.setText("you typed: " + str);
-    	AlertDialog.Builder builder = new AlertDialog.Builder(this);
-    	builder.setTitle("Message");
-    	builder.setMessage("change activity.");
-    	builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			
-			public void onClick(DialogInterface dialog, int which) {
-				Intent intent = new Intent(activity, com.tsutsu.isbn9784798031996.OtherActivity.class);
-				activity.startActivity(intent);
-			}
-		});
-    	builder.setNegativeButton("Cancel", null);
-    	builder.show();
+    	String url = "http://" + edit1.getText();
+    	Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+    	this.startActivity(intent);
     }
 }
