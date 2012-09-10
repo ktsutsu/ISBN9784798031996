4374e0f832fea053ad7ee72bdb820577873ab762
ktsutsu <tsutsu0422@googlemail.com>
2012-09-10 12:55:46 +0900

    6.2.2 Intentから値を受け取る

--- src/com/tsutsu/isbn9784798031996/OtherActivity.java
+++ src/com/tsutsu/isbn9784798031996/OtherActivity.java
@@ -3,7 +3,7 @@ package com.tsutsu.isbn9784798031996;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.TextView;
 
 public class OtherActivity extends Activity {
 
@@ -11,10 +11,11 @@ public class OtherActivity extends Activity {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.other);
-	}
-
-	public void doAction(View view) {
-		Intent intent = new Intent(this, com.tsutsu.isbn9784798031996.MainActivity.class);
-		this.startActivity(intent);
+		Intent intent = this.getIntent();
+		if (intent.getAction().equals(Intent.ACTION_SEND)) {
+			Bundle bundle = intent.getExtras();
+			TextView text1 = (TextView)this.findViewById(R.id.otherTextView1);
+			text1.setText(bundle.getCharSequence(Intent.EXTRA_TEXT));
+		}
 	}
 }
