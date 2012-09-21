2fcd2a98aaf8b1c65c434f7acd38a8f963b90ca8
ktsutsu <tsutsu0422@googlemail.com>
2012-09-21 19:17:35 +0900

    6.3.2 ノーティフィケーションを作ってみる

--- src/com/tsutsu/isbn9784798031996/MainActivity.java
+++ src/com/tsutsu/isbn9784798031996/MainActivity.java
@@ -8,7 +8,8 @@ import android.widget.*;
 import android.content.*;
 
 public class MainActivity extends Activity {
-	static final int MAINACTIVITY_CODE = 0;
+	static final int ACTIVITY_CODE = 0;
+	static final int NOTIFY_ID = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,33 +19,15 @@ public class MainActivity extends Activity {
 
     public void doAction(View view) {
     	EditText edit1 = (EditText)this.findViewById(R.id.editText1);
-    	EditText edit2 = (EditText)this.findViewById(R.id.editText2);
-    	EditText edit3 = (EditText)this.findViewById(R.id.editText3);
-    	Editable name1 = edit1.getText();
-    	Editable name2 = edit2.getText();
-    	Editable age = edit3.getText();
-    	MyData data = new MyData(name1.toString(), name2.toString(), age.toString());
-    	Intent intent = new Intent(Intent.ACTION_SEND);
-    	intent.setType("application/mydata");
-    	intent.putExtra("MyData", data);
-    	this.startActivityForResult(intent, MAINACTIVITY_CODE);
-    }
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    	super.onActivityResult(requestCode, resultCode, data);
-    	if (requestCode == MAINACTIVITY_CODE) {
-			String result = "";
-			try {
-				result = (String)data.getExtras().getCharSequence(Intent.EXTRA_TEXT);
-			} catch (NullPointerException e) {
-				result = e.getMessage();
-			}
-			Toast toast = Toast.makeText(this, result, Toast.LENGTH_SHORT);
-			toast.show();
-		} else {
-			Toast toast = Toast.makeText(this, "no result...", Toast.LENGTH_SHORT);
-			toast.show();
-		}
+    	Editable s = edit1.getText();
+    	Intent intent = new Intent(this, com.tsutsu.isbn9784798031996.MainActivity.class);
+    	PendingIntent pending = PendingIntent.getActivity(this, ACTIVITY_CODE, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+    	Notification notify = new Notification();
+    	notify.flags = Notification.FLAG_AUTO_CANCEL;
+    	notify.icon = R.drawable.ic_launcher;
+    	notify.tickerText = "HelloApp notification!!";
+    	notify.setLatestEventInfo(this, "HelloApp Info", "message: " + s, pending);
+    	NotificationManager manager = (NotificationManager)this.getSystemService(Activity.NOTIFICATION_SERVICE);
+    	manager.notify(NOTIFY_ID, notify);
     }
 }
